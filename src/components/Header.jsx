7a985a7858dca7c70476e4346995443d45e79678
Pankaj Kumar Dhal <pk99dhal@gmail.com>
2024-11-01 02:53:23 +0530

    add help functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,68 @@
-import React from "react";
+// import React from "react";
+// import styles from "../css/Header.module.css";
+// import { IoSearchSharp } from "react-icons/io5";
+// import { MdFormatAlignCenter } from "react-icons/md";
+// import { IoIosHelpCircle, IoMdSettings, IoMdApps } from "react-icons/io";
+// import { MdOutlineHelpOutline } from "react-icons/md";
+
+
+// function Header({ photoURL, setSearchTerm }) {
+//   const handleSearchChange = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <div className={styles.header}>
+//       <div className={styles.header__logo}>
+//         <img
+//           src="https://static-00.iconduck.com/assets.00/google-drive-icon-2048x2048-j5sa1hcp.png"
+//           alt=""
+//         />
+//         <span>Drive</span>
+//       </div>
+//       <div className={styles.header__search}>
+//         <IoSearchSharp />
+//         <input
+//           type="text"
+//           placeholder="Search in Drive"
+//           onChange={handleSearchChange}
+//         />
+//         <MdFormatAlignCenter />
+//       </div>
+//       <div className={styles.header__icons}>
+//         <span><MdOutlineHelpOutline />
+//         </span>
+//         <span><IoMdSettings /></span>
+//         <span>
+//           <IoMdApps />
+//           <img src={photoURL} alt="User Profile" className={styles.profilePicture} />
+//           {/* <img src={photoURL || "https://via.placeholder.com/32"} alt="User Profile" className={styles.profilePicture} /> */}
+
+//         </span>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+import React, { useState } from "react";
 import styles from "../css/Header.module.css";
 import { IoSearchSharp } from "react-icons/io5";
 import { MdFormatAlignCenter } from "react-icons/md";
-import { IoIosHelpCircle, IoMdSettings, IoMdApps } from "react-icons/io";
+import { IoMdSettings, IoMdApps } from "react-icons/io";
+import { MdOutlineHelpOutline } from "react-icons/md";
+import HelpModal from "./HelpModal"; // Import the modal component
 
 function Header({ photoURL, setSearchTerm }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
   return (
@@ -14,7 +70,7 @@ function Header({ photoURL, setSearchTerm }) {
       <div className={styles.header__logo}>
         <img
           src="https://static-00.iconduck.com/assets.00/google-drive-icon-2048x2048-j5sa1hcp.png"
-          alt=""
+          alt="Drive Logo"
         />
         <span>Drive</span>
       </div>
@@ -28,15 +84,16 @@ function Header({ photoURL, setSearchTerm }) {
         <MdFormatAlignCenter />
       </div>
       <div className={styles.header__icons}>
-        <span><IoIosHelpCircle /></span>
+        <span onClick={toggleModal}>
+          <MdOutlineHelpOutline />
+        </span>
         <span><IoMdSettings /></span>
         <span>
           <IoMdApps />
           <img src={photoURL} alt="User Profile" className={styles.profilePicture} />
-          {/* <img src={photoURL || "https://via.placeholder.com/32"} alt="User Profile" className={styles.profilePicture} /> */}
-
         </span>
       </div>
+      {isModalOpen && <HelpModal toggleModal={toggleModal} />} {/* Show modal if open */}
     </div>
   );
 }
